df355853ad40a5d0ed1d4cc8853ca95217049600
basila1 <basila_n@outlook.com>
2017-04-10 17:46:41 -0700

    completed math module

---
+++ math_module/app.js
@@ -0,0 +1,5 @@
+var math = require('./mathlib')();     //notice the extra invocation parentheses!
+console.log(math.add(6,7));
+console.log(math.multiply(6,7));
+console.log(math.square(7));
+console.log(math.random(7,10));
