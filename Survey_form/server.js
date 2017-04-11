fbabbd8f3fffc6c77aeffea8deafa5ce0cad0fef
basila1 <basila_n@outlook.com>
2017-04-11 12:59:18 -0700

    Completed Survey Form

---
+++ Survey_form/server.js
@@ -0,0 +1,50 @@
+
+// require express
+var express = require("express");
+// path module -- try to figure out where and why we use this
+var path = require("path");
+// create the express app
+var app = express();
+var bodyParser = require('body-parser');
+var user;
+
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+ res.render("index");
+})
+//
+// app.post('/back', function (req, res){
+//   res.redirect('/');
+// });
+
+app.get('/results', function (req, res){
+  res.render('results', {users: user});
+});
+// post route for adding a user
+app.post('/users', function(req, res) {
+  user = req.body
+  console.log("POST DATA", user);
+  res.redirect('/results');
+
+app.get("/users", function (req, res) {
+  user = req.body
+  response.render('users', {users: user});
+})
+
+
+
+//  // This is where we would add the user to the database
+//  // Then redirect to the root route
+//  res.redirect('/');
+})
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
