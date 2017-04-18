94aeedd9f62f54ab59878baaeed6f63851a58049
basila1 <basila_n@outlook.com>
2017-04-17 21:34:03 -0700

    custom users

---
+++ Angular/Partials_and_Custom_Users/client/main.js
@@ -0,0 +1,73 @@
+//  inject the ngRoute dependency in the module.
+var myApp = angular.module('myApp', ['ngRoute']);
+
+//: Create user factory
+myApp.factory('userFactory', function() {
+    var people = [
+        {first_name: 'Basila', last_name: 'Nathan', fav_lang: 'Ruby'},
+        {first_name: 'Saloni', last_name: 'Madan', fav_lang: 'JavaScript'},
+        {first_name: 'Manish', last_name: 'Badwal', fav_lang: 'JavaScript'},
+        {first_name: 'Tanjeev', last_name: 'Thandi', fav_lang: 'Python'}
+    ];
+    var factory = {};
+    //: Index method to return people
+    factory.getAll = function(callback) {
+        callback(people);
+    }
+    //: Add method to add person
+    factory.add = function(person) {
+        people.push(person);
+    }
+    //: Delete method to remove person
+    factory.remove = function(person) {
+        people.splice(people.indexOf(person), 1);
+    }
+    //: Return the object
+    return factory;
+});
+
+//  use the config method to set up routing:
+myApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/',{
+        templateUrl: 'partials/customizeUsers.html',
+        controller: 'CustomizeUsersController'
+    })
+    .when('/users',{
+        templateUrl: 'partials/userList.html',
+        controller: 'UserListsController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+//  build the controllers
+myApp.controller('CustomizeUsersController', ['$scope', 'userFactory', '$location', function ($scope, userFactory, $location) {
+    console.log($location);
+    $scope.sortType = 'first_name';
+    $scope.sortReverse = false;
+    $scope.people = [];
+    userFactory.getAll(function (data) {
+        $scope.people = data;
+    });
+    $scope.addPerson = function() {
+        if($scope.newPerson) {
+            userFactory.add($scope.newPerson);
+            $scope.newPerson = {};
+            $location.url('/users')
+        }
+    };
+    $scope.delPerson = function(person) {
+        userFactory.remove(person);
+    };
+}]);
+
+myApp.controller('UserListsController', ['$scope', 'userFactory', function ($scope, userFactory) {
+    $scope.sortType = 'first_name';
+    $scope.sortReverse = false;
+    $scope.people = [];
+    userFactory.getAll(function (data) {
+        $scope.people = data;
+    });
+}]);
