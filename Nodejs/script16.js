2c54211b5faa07103c91a87107a6045508fa4480
basila1 <basila_n@outlook.com>
2017-05-23 16:50:16 -0700

    http server 2

--- Nodejs/script16.js
+++ Nodejs/script16.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var app = express();
 
 app.use('/cssFiles', express.static(__dirname + '/assets'));
 
 app.get('/', function (req, resp) {
   resp.sendFile('index.html', {root: path.join(__dirname, './files')});
+});
+
+app.get(/^(.+)$/, function (req, resp) { //regular expression matches any file that begins with one or more characters
+  console.log(req.params);
+  //then it will try to run this code
+  try {
+    //if it is a file indeed -> isFile() then throw the file
+    if (fs.statSync(path.join(__dirname, './files', req.params[0]+'.html')).isFile()) {
+      resp.sendFile(req.params[0]+'.html', {root: path.join(__dirname, './files')});
+    }
+    //else catch the error, and send the 404.html file.
+  } catch(err) {
+    resp.sendFile('404.html', {root: path.join(__dirname, './files')});
+  }
 });
 
 app.listen(1337, function () {
