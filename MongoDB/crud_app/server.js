d2c47b3bfcd6572c4c841f068e0cf2b0b95dfec9
basila1 <basila_n@outlook.com>
2017-05-24 14:41:10 -0700

    done setting up database

--- MongoDB/crud_app/server.js
+++ MongoDB/crud_app/server.js
@@ -1,6 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb');
+
+
+//we want to start our servers only when the database is connected. Hence, let’s move app.listen into the connect method. We’re also going to create a db variable to allow us to use the database when we handle requests from the browser.
+MongoClient.connect('mongodb://abcd:abcd@ds151661.mlab.com:51661/star-wars-quotes', (err, database) => {
+  if (err) {
+    console.log(err);
+  }
+  db = database;
+  app.listen(3000, function () {
+    console.log("listeing on port 3000!!");
+  });
+});
 
 //The urlencoded method within body-parser tells body-parser to extract data from the <form> element and add them to the body property in the request object.
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,8 +26,4 @@ app.get('/', (req, res) => {
 //Now, you should be able to see everything in the form field within the req.body object.
 app.post('/quotes', (req, res) => {
   console.log(req.body);
-});
-
-app.listen(3000, function () {
-  console.log("listeing on port 3000!!");
 });
